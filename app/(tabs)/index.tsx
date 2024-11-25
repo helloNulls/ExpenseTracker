f5b0d4848b855b6a9482fd173f5462fb512b7b01
hyewon6588 <hhan29@my.centennialcollege.ca>
2024-11-24 19:56:31 -0500

    converted login view from jsx to tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,70 +1,130 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import React, { useState } from 'react';
+import { 
+  Image, 
+  View, 
+  TouchableOpacity, 
+  Text, 
+  TextInput, 
+  ScrollView, 
+  KeyboardAvoidingView, 
+  Platform, 
+  Alert 
+} from 'react-native';
+import { NavigationProp } from '@react-navigation/native';
+import styles from '../../styles/loginScreenStyles';
+import Feather from '@expo/vector-icons/Feather';
+import { LinearGradient } from 'expo-linear-gradient';
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+type RootStackParamList = {
+  ForgotPassword: undefined;
+  signin: undefined;
+};
 
-export default function HomeScreen() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
-  );
+type LoginScreenNavigationProp = NavigationProp<RootStackParamList, 'signin'>;
+
+interface LoginProps {
+  navigation: LoginScreenNavigationProp;
 }
 
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
+const Login: React.FC<LoginProps> = ({ navigation }) => {
+  const googleImg = require('../../assets/images/google.png');
+  const facebookImg = require('../../assets/images/facebook.png');
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const handleLogin = async () => {
+    try {
+      const response = await fetch('http://localhost:5002/signin', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ email, password }),
+      });
+
+      const data = await response.json();
+      console.log('Response data:', data);
+
+      if (response.ok) {
+        console.log('Login Success');
+        Alert.alert('Login Success', data.message);
+      } else {
+        Alert.alert('Login Failed', data.message);
+        console.log('Login Failed');
+      }
+    } catch (error) {
+      Alert.alert('Cannot connect to Server');
+    }
+  };
+
+  return (
+    <LinearGradient
+      colors={['#9dd1eb', '#6dd9e3']}
+      start={{ x: 0, y: 1 }}
+      end={{ x: 1, y: 0 }}
+      style={styles.container}
+    >
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      >
+        <ScrollView contentContainerStyle={styles.inner}>
+          <View style={styles.imgCon}></View>
+          <Text style={styles.title}>Welcome Back!</Text>
+          <View style={styles.innerContainer}>
+            <Text style={styles.conText}>Email</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Enter your email"
+              value={email}
+              onChangeText={setEmail}
+            />
+            <Text style={styles.conText}>Password</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Enter your password"
+              value={password}
+              onChangeText={setPassword}
+              secureTextEntry
+            />
+            <View style={styles.loginCon}>
+              <TouchableOpacity
+                onPress={() => navigation.navigate('ForgotPassword')}
+                style={styles.forgetPWContainer}
+              >
+                <Text style={styles.forgetPW}>Forgot Password?</Text>
+              </TouchableOpacity>
+            </View>
+
+            <TouchableOpacity style={styles.pressButton} onPress={handleLogin}>
+              <Text style={styles.login}>Login</Text>
+              <Feather name="log-in" size={28} color="black" />
+            </TouchableOpacity>
+          </View>
+
+          <View style={styles.otherLoginContainer}>
+            <View style={styles.line} />
+            <Text style={styles.signinwith}>Or Sign-In With</Text>
+            <View style={styles.line} />
+          </View>
+
+          <TouchableOpacity style={styles.otherAccount}>
+            <Text style={styles.loginText}>Google Account</Text>
+            <Image source={googleImg} style={styles.logoImg} />
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.otherAccount}>
+            <Text style={styles.loginText}>Facebook Account</Text>
+            <Image source={facebookImg} style={styles.logoImg} />
+          </TouchableOpacity>
+
+          <View style={styles.registerBox}>
+            <Text>Don't have an account?</Text>
+            <TouchableOpacity onPress={() => navigation.navigate('signin')}>
+              <Text style={styles.register}>Register</Text>
+            </TouchableOpacity>
+          </View>
+        </ScrollView>
+      </KeyboardAvoidingView>
+    </LinearGradient>
+  );
+};
+
+export default Login;
