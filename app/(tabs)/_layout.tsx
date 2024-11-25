c0c8e65b9f920e980b98636de8efb09dfbeeb14f
YeoulKang <ykang35@my.centennialcollege.ca>
2024-11-24 19:34:01 -0500

    Add overview tab and move index.tsx to overview

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -32,6 +32,26 @@ export default function TabLayout() {
           ),
         }}
       />
+  <Tabs.Screen
+  name="profile"
+  options={{
+    title: 'Profile',
+    tabBarIcon: ({ color, focused }) => (
+      <TabBarIcon name={focused ? 'person' : 'person-outline'} color={color} />
+    ),
+  }}
+/>
+<Tabs.Screen
+  name="overview"
+  options={{
+    title: 'Overview',
+    tabBarIcon: ({ color, focused }) => (
+      <TabBarIcon name={focused ? 'analytics' : 'analytics-outline'} color={color} />
+    ),
+  }}
+/>
+
+
     </Tabs>
   );
 }
